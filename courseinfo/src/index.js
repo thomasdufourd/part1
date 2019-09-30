6feb9970bd704f9587350d832773da7ec5c007e8
Thomas Dufourd <thomas.dufourd@gmail.com>
2019-09-30 19:52:23 +0200

    Refactor: add Part(s)

--- courseinfo/src/index.js
+++ courseinfo/src/index.js
@@ -1,27 +1,54 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// const-definitions:
+const course = 'Half Stack application development';
+const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+const Header = (props) => {
+    return (
+        <h1>
+            {props.courseName}
+        </h1>
+    );
+}
+
+const Part = (props) => {
+    console.log('received props.name: ' + props.name);
+    return (
+        <p>
+            {props.name} {props.exercises}
+        </p>
+    );
+
+}
+
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
   const exercises1 = 10
-  const part2 = 'Using props to pass data'
+
   const exercises2 = 7
-  const part3 = 'State of a component'
+  
   const exercises3 = 14
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Header courseName={course}/>
+      <Part name={part1.name} exercises={part1.exercises}/>
+      <Part name={part2.name} exercises={part2.exercises}/>
+      <Part name={part3.name} exercises={part3.exercises}/>
+
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
